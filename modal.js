ee601d249b90ab431054865368ba0e86d20d0874
Hannah Isaacson <hisaacson21@gmail.com>
2019-11-24 20:46:56 -0500

    update modal.js
    added back to top button

--- modal.js
+++ modal.js
@@ -25,4 +25,33 @@ jQuery(document).ready(function() {
         //Close modal with the close the modal button:
         $(".modal-close-button").on('click', closeModal);
     });
+
+
+//Function for Back to Top Button
+
+    //get the button:
+    mybutton = document.getElementById("myButton");
+
+    //When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function(){
+        scrollFunction()};
+
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+            mybutton.style.display = "block";
+        }else {
+            mybutton.style.display = "none";
+        }
+    }
+
+
+    //When the user clicks on the button, scroll to the top of the page
+    $(mybutton).on("click", function(){
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+
+
+
 });
